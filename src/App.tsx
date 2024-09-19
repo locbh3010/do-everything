762045a018e10a34ceeefbc9a862a18768e2745f
locbh <locbh3010@gmail.com>
2024-09-20 02:03:19 +0700

    add alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { Button, Tooltip } from '@mantine/core';
-
 const App = () => {
-  return (
-    <div className="text-white uppercase flex-col flex-between-center">
-      <h1 className="font-bold text-2xl">Hello world!!</h1>
-      <Tooltip label="abc">
-        <Button className="active:!translate-y-0">Click me</Button>
-      </Tooltip>
-    </div>
-  );
+  return <></>;
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import UnoCSS from 'unocss/vite';
+import { ViteAliases } from 'vite-aliases';
+import vitePluginImp from 'vite-plugin-imp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [UnoCSS(), react()],
+  plugins: [
+    ViteAliases({ prefix: '@', useConfig: true, adjustDuplicates: true }),
+    vitePluginImp({
+      libList: [
+        { libName: 'lodash', libDirectory: '', camel2DashComponentName: false },
+      ],
+      exclude: ['antd'],
+    }),
+    UnoCSS(),
+    react(),
+  ],
 });
